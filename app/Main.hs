a19acac8511222a0b1b1e46b5c7810e2434c4919
tomasmaiza <tomimaiza@gmail.com>
2025-02-27 16:36:47 -0300

    Agrego código al main

--- app/Main.hs
+++ app/Main.hs
@@ -9,4 +9,12 @@ import           System.Exit
 import           Control.Monad                  ( when )
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- Env.getArgs
+  case args of
+    [filename] -> do input <- readFile filename
+                     case parseComm filename input of
+                      Left err -> putStrLn $ "Error de parsing:\n" ++ show err
+                      Right ast -> do let result = eval ast
+                                      print result
+    _ -> putStrLn "Uso: ./programa archivo.frl"
